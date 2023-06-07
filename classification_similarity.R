c35c0730485c0a73753d01d7edb8622a6b5c83db
Konstantin <konsti999@gmx.de>
2023-06-07 11:20:34 +0200

    distance calculations

--- classification_similarity.R
+++ classification_similarity.R
@@ -6,7 +6,6 @@ raw20 <- rast("image_export_raw2020.tif")
 class20 <- rast("image_export_classified2020.tif")
 
 raw21 <- rast("image_export_raw2021.tif")
-mean(raw21)
 
 
 class_mean_vectors <- NULL
@@ -17,17 +16,28 @@ for(c in 1:9){
   }
   class_mean_vectors <- rbind(class_mean_vectors, bands_mean)
 }
-class_mean_vectors <- as.data.frame(class_mean_vectors)
-colnames(class_mean_vectors) <- c("B1", "B2", "B3", "B4", "B5", "B6")
 
-vec21 <- c(
-  values(raw21[[1]])[6672],
-  values(raw21[[2]])[6672],
-  values(raw21[[3]])[6672],
-  values(raw21[[4]])[6672],
-  values(raw21[[5]])[6672],
-  values(raw21[[6]])[6672]
-)
+euclidean_dist <- distance(raw21, class_mean_vectors[1, ])
 
-# Calculate the eucldean distance from vec21 to all vectors in the dataframe to receive vec21s class
+classification_vector <- c()
+for(t in seq_along(length(values(raw21)))){
+  shortest_distance <- 999999999
+
+  for(i in 1:9){
+    b_vec <- class_mean_vectors[i,]
+    euclidean_distance <- dist(rbind(b_vec, extract(raw21, t)[-1]))
+
+    if(!is.na(euclidean_distance)){
+      if(euclidean_distance < shortest_distance){
+        shortest_distance <- euclidean_distance
+        best_class <- i
+      }
+    }else{
+      best_class <- NA
+    }
+
+  }
+  print(best_class)
+  classification_vector <- append(classification_vector, best_class)
+}
 
